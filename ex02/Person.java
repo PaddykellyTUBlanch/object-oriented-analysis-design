77c90ac74c53254b33c187fafaa4b33b8ec1f7e8
Mark Deegan <mark.deegan@tudublin.ie>
2023-10-25 14:55:10 +0100

    Changing file names to Person.java and Student.java

---
+++ ex02/Person.java
@@ -0,0 +1,13 @@
+/**
+Paddy Kelly <br>
+Wednesday 20231025 1110 <br>
+inherintance person <br>
+Sample to demonstrate basic java concepts
+*/
+public class person 
+{ // start declaration of the class HelloPerson
+	public static void main(String[] args)
+    	{ // start declaration of main method
+		System.out.println("hello person"); // standard Hello person! greeting
+	} // end declaration of main method
+} // end declaration of class HelloPerson
